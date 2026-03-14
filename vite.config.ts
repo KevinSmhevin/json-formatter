6dfba8556fdd51c81d8a9d9ad4fd7f6381ec257c
KevinSmhevin <KevinSmhevin@users.noreply.github.com>
2026-03-14 15:35:37 -0700

    adds to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const pagesBase = repositoryName ? `/${repositoryName}/` : '/'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: pagesBase,
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
   ],
 })
